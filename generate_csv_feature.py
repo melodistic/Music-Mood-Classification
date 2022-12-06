c0234261870dcaa65fb7c972c066d43035fb8bec
GGolfz <wisarutgolf11@gmail.com>
2022-12-06 22:25:04 +0700

    change value

--- generate_csv_feature.py
+++ generate_csv_feature.py
@@ -20,5 +20,5 @@ for mood in mood_list:
             with open(song[3], "r") as f:
                 data = json.load(f)
                 new_df.append([song[1],*data])
-        new_df = pd.DataFrame(new_df, columns=["music_name", *["feature_"+str(i) for i in range(1280)]])
+        new_df = pd.DataFrame(new_df, columns=["music_name", *["feature_"+str(i) for i in range(512)]])
         new_df.to_csv(f"features/csv_features/{mood}-{bpm_mode}.csv", index=False)
